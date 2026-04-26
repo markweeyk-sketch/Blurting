e8df4ddf1d2128db80db497f5997879d34e65c57
markweeyk-sketch <markweeyk@gmail.com>
2026-04-26 17:26:19 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
+  './android-chrome-192x192.png',
+  './android-chrome-512x512.png',
 ];
 
 self.addEventListener('install', e => {
